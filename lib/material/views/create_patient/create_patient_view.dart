485ed9c99c3dc58c97c60997b4a06830134756a4
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2021-08-24 11:23:44 +0530

    added create_patient_view.dart

---
+++ lib/material/views/create_patient/create_patient_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreatePatientView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Text('Create patient view'),
+        ),
+      ),
+    );
+  }
+}
