613dff85508c6345ef1b2382786e1b72406c2008
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2021-08-28 10:09:23 +0530

    added table_calendar

--- lib/cupertino/views/patient_search/patient_search_view.dart
+++ lib/cupertino/views/patient_search/patient_search_view.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:surgery_support/cupertino/views/create_patient/personal_info_view.dart';
 
 class PatientSearchView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: Text('Patient search'),
+    return CustomScrollView(
+      slivers: [
+        CupertinoSliverNavigationBar(
+          largeTitle: Text('Search'),
+          trailing: GestureDetector(
+            onTap: () => showCupertinoModalBottomSheet(
+                useRootNavigator: true,
+                elevation: 5.0,
+                expand: true,
+                context: context,
+                builder: (context) => PersonalInfoView()),
+            child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Icon(CupertinoIcons.person_add)),
+          ),
+        ),
+      ],
     );
   }
 }
