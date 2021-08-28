7f2c3a8af1b6416494da790a4fcd0935fc882686
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2021-08-28 22:24:25 +0530

    setting up patient search view and list items and added profile view

--- lib/material/views/patient_search/patient_search_view.dart
+++ lib/material/views/patient_search/patient_search_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wardeleven/material/widgets/patient_list_item.dart';
 
 class PatientSearchView extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -9,21 +9,28 @@ class PatientSearchView extends StatelessWidget {
       child: SafeArea(
         child: CustomScrollView(
           slivers: [
-            SliverAppBar(
-                title: Text('Patient search'),
-                actions: <Widget>[
-                  IconButton(
-                    icon: const Icon(Icons.search),
-                    tooltip: 'Search',
-                    onPressed: () { /* ... */ },
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.filter_alt),
-                    tooltip: 'Search',
-                    onPressed: () { /* ... */ },
-                  ),
-                ]
-            ),
+            SliverAppBar(title: Text('Patient search'), actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.search),
+                tooltip: 'Search',
+                onPressed: () {
+                  /* ... */
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.filter_alt),
+                tooltip: 'Search',
+                onPressed: () {
+                  /* ... */
+                },
+              ),
+            ]),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => PatientListItem(),
+                childCount: 10,
+              ),
+            )
           ],
         ),
       ),
